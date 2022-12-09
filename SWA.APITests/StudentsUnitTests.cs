45eefd889e83753a7b9c7c009f581349691de314
Elio Cortes <elio.cortes3000@gmail.com>
2022-12-09 15:02:04 -0500

    Implemented Minimal API Service

---
+++ SWA.APITests/StudentsUnitTests.cs
@@ -0,0 +1,83 @@
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.VisualStudio.TestPlatform.TestHost;
+using Newtonsoft.Json;
+using SWA.DataAccess.Models;
+using System.Net;
+using System.Net.Http.Json;
+
+namespace SWA.APITests
+{
+    public class StudentsUnitTests
+    {
+        [Fact]
+        public async void GetStudentById()
+        {
+            await using var application = new WebApplicationFactory<Program>();
+            using var client = application.CreateClient();
+            var response = await client.GetAsync($"/api/students/{1}");
+            var data = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async void GetAllStudents()
+        {
+            await using var application = new WebApplicationFactory<Program>();
+            using var client = application.CreateClient();
+            var response = await client.GetAsync("/api/students");
+            var data = await response.Content.ReadAsStringAsync();
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+
+        [Fact]
+        public async void CreateStudent()
+        {
+            await using var application = new WebApplicationFactory<Program>();
+            using var client = application.CreateClient();
+            Student student = new Student()
+            {
+                Id = 6,
+                Age = 45,
+                Career = "Doctor",
+                FirstName = "Jacobo",
+                LastName = "Burton",
+                Username = "Jack"
+            };
+            var content = JsonConvert.SerializeObject(student);
+            var response = await client.PostAsJsonAsync("/api/students", content);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async void UpdateStudent()
+        {
+            await using var application = new WebApplicationFactory<Program>();
+            using var client = application.CreateClient();
+            Student student = new Student()
+            {
+                Id = 6,
+                Age = 45,
+                Career = "Lawyer",
+                FirstName = "Jacobo",
+                LastName = "Burton",
+                Username = "Jackie"
+            };
+            var content = JsonConvert.SerializeObject(student);
+            var response = await client.PutAsJsonAsync("/api/students", content);
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+        [Fact]
+        public async void DeleteStudent()
+        {
+            await using var application = new WebApplicationFactory<Program>();
+            using var client = application.CreateClient();
+            Student student = new Student();
+            var content = JsonConvert.SerializeObject(student);
+            var response = await client.DeleteAsync($"/api/students/{6}");
+            Assert.Equal(HttpStatusCode.OK, response.StatusCode);
+        }
+
+    }
+}
---
+++ SWA.DataAccess/Data/StudentsContext.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SWA.DataAccess.Models;
+
+namespace SWA.DataAccess.Data;
+
+public partial class StudentsContext : DbContext
+{
+    //protected readonly IConfiguration Configuration;
+
+    //public StudentsContext(IConfiguration configuration)
+    //{
+    //    Configuration = configuration;
+    //}
+
+
+    //protected override void OnConfiguring(DbContextOptionsBuilder options)
+    //{
+    //    // connect to sqlite database
+    //    options.UseSqlite(Configuration.GetConnectionString("StudentsDBConnection"));
+    //}
+
+    // public StudentsContext(DbContextOptions<StudentsContext> options)
+    // : base(options)
+    // {
+    // }
+
+    public virtual DbSet<Student> Students { get; set; } = null!;
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Student>(entity =>
+        {
+            entity.ToTable("Student");
+
+            entity.HasIndex(e => e.Username, "IX_Student_Username").IsUnique();
+
+            entity.Property(e => e.Career).HasColumnType("VARCHAR(50)");
+            entity.Property(e => e.FirstName).HasColumnType("VARCHAR(20)");
+            entity.Property(e => e.LastName).HasColumnType("VARCHAR(20)");
+            entity.Property(e => e.Username).HasColumnType("VARCHAR(20)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
